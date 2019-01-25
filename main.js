00a4dec267099d3f76493fc42d1781556923c156
Berrie Trippe <berrietrippe@gmail.com>
2019-01-25 11:05:54 +0100

    Add energy production graph

--- main.js
+++ main.js
@@ -1,30 +1,96 @@
+/*
 
-data_list = [];
+ */
 
-let height = $("#o-chart-1").height() - 52;
-let width = $("#o-chart-1").width() - 20;
+
+
 
 d3.csv("data/Energiebalans__aanbod__verbruik_17012019_164739.csv", function(data){
+    let parsedData = parseData(data);
+    drawChart1(parsedData);
+});
+
+function parseData(data){
+    let arr = [];
     for (let i = 0; i < data.length; i++){
-        var val = data[i]["Energieaanbod/Winning (PJ)"];
-        console.log(val);
-        data_list[i] = parseInt(val);
+        let value = data[i]["Energieaanbod/Winning (PJ)"];
+        let date = new Date(data[i]["Perioden"]);
+        arr.push(
+            {
+                date: date,
+                value: + value
+            });
     }
 
-    var x = width/data_list.length;
+    return arr;
+}
+
+function drawChart(data){
+    var x = width/data.length;
 
     var y = d3.scale.linear()
-        .domain([0, d3.max(data_list)])
+        .domain([0, d3.max(data)])
         .range([0, height]);
-
-
 
     d3.select(".chart")
         .selectAll("div")
-        .data(data_list)
+        .data(data)
         .enter().append("div")
         .style("height", function(d) { return y(d) + "px"; })
         .style("width", function(d) { return x + "px"; })
         .style("background-color", function(d) { return "#007bff"})
         .text(function(d) { return d; });
-});
+}
+
+
+function drawChart1(data) {
+    let svgWidth = $("#o-line-chart-1").width();
+    let svgHeight = $("#o-line-chart-1").height();
+    let margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    let width = svgWidth - margin.left - margin.right;
+    let height = svgHeight - margin.top - margin.bottom;
+    // var svgWidth = 1000, svgHeight = 600;
+    let svg = d3.select('svg')
+        .attr("width", width)
+        .attr("height", height);
+
+    let g = svg.append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")"
+        );
+
+    let x = d3.scaleTime().rangeRound([0, width]);
+    let y = d3.scaleLinear().rangeRound([height, 0]);
+
+    let line = d3.line()
+        .x(function(d) { return x(d.date)})
+        .y(function(d) { return y(d.value)});
+    x.domain(d3.extent(data, function(d) { return d.date }));
+    y.domain(d3.extent(data, function(d) { return d.value }));
+
+
+    g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .select(".domain");
+        // .remove();
+
+    g.append("g")
+        .call(d3.axisLeft(y))
+        .append("text")
+        .attr("fill", "#000")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Production (PJ)");
+
+    g.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
+}
